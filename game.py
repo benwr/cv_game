852c3eb5da439f61fd588ff9e01dfc2b50196e01
Ben Cohen <bn.chn2@gmail.com>
2013-10-05 16:13:27 -0400

    contour working

--- game.py
+++ game.py
@@ -1,24 +1,26 @@
-import cv
-import cv2
 from SimpleCV import *
 import pygame
 import itertools
 import time
 def main():
 	
-		simplecvimg = Image('board1.JPG')
+		simplecvimg = Image('board2.JPG')
 
 
-		#640x480
-		blue = simplecvimg.colorDistance((2,7,63)) * 1.5  #scale up
+		# blue = simplecvimg.colorDistance((2,7,63)) * 2  #scale up
+		blue = simplecvimg.colorDistance((2,5,55)) * 1.5  #scale up
+
 		red = simplecvimg.colorDistance((62,5,13)) 
 
 
 		l1 = DrawingLayer((simplecvimg.width, simplecvimg.height))
 
+		blue.show()
 
+		cv.WaitKey(1000)
 		redBlobs = (simplecvimg - red).findBlobs(minsize=200)
 		blueLine = (simplecvimg - blue).findBlobs()
+		
 
 		simplecvimg.addDrawingLayer(l1)
 		simplecvimg.applyLayers()
@@ -35,8 +37,29 @@ def main():
 
 
 		if blueLine != None:
-			blueLine.show()
-			cv.WaitKey(5000)
+			blueLine[0].drawOutline(layer=l1,color=Color.RED,width=3,alpha=128)
+
+			# blue = blueLine[0].blobMask()
+		c = blueLine[0].contour()
+		
+		# d = blue.fitContour(c)
+		startPoint = (1000,10000)
+		for x in c:
+			if x[0] < startPoint[0]:
+				startPoint = x
+		print startPoint
+
+
+
+		importantPoints = [startPoint]
+		for point in c:
+			y_delta = point[1]-importantPoints[-1][1]
+			if point[0] > importantPoints[-1][0] and (y_delta > 10 or y_delta<-10):
+				importantPoints.append(point)
+
+		for p in importantPoints:
+			l1.circle(p, 10)
+
 		blue.addDrawingLayer(l1)
 		blue.applyLayers()
 		blue.show()
